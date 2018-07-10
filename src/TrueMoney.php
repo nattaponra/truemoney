bedc92a2d292fb5b49137e29fac69d42a7bbe256
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-07-10 14:15:01 +0700

    Add comment

--- src/TrueMoney.php
+++ src/TrueMoney.php
@@ -1,15 +1,41 @@
 <?php
 namespace nattaponra\truemoney;
 
+use nattaponra\truemoney\Services\TMPay;
+
 Class TrueMoney{
 
-    public function __construct(ServiceInterface $service)
+
+    /**
+     * @var \nattaponra\truemoney\ServiceInterface
+     */
+    private $service;
+
+
+    /**
+     * @var string
+     */
+    private $defaultServiceName;
+
+
+
+    public function __construct()
     {
+        $this->defaultServiceName = config("truemoney.default_service");
+
+        if($this->defaultServiceName == "tmpay"){
+            $this->service = new TMPay();
+        }
 
     }
 
-    public function charge(){
-
+    /**
+     * Charge money from true money number via service.
+     * @param  string   $trueMoneyNumber
+     * @return boolean
+     */
+    public function charge($trueMoneyNumber){
+        return $this->service->charge($trueMoneyNumber);
     }
 }
 
