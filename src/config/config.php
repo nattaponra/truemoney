5f72e60760b863bffe418e22c5a6cf96529d2f3e
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-07-10 15:31:33 +0700

    Modify config

--- src/config/config.php
+++ src/config/config.php
@@ -2,5 +2,11 @@
 
 
 return [
-    'default_service' => env("TRUE_MONEY_SERVICE","tmpay")
+    'default_service' => env("TRUE_MONEY_SERVICE","tmpay"),
+    'services' => [
+        'tmpay' => [
+            'merchant_id' => env("TMPAY_MERCHANT_ID",null),
+            'resp_url'    => env("TMPAY_RESP_URL",'http://localhost/callback.php'),
+        ]
+    ]
 ];
