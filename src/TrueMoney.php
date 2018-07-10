6bff8a66fab251a6b0593b9868993e0c9332e5c9
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-07-10 13:37:21 +0700

    Create main class

---
+++ src/TrueMoney.php
@@ -0,0 +1,16 @@
+<?php
+namespace nattaponra\truemoney;
+
+Class TrueMoney{
+
+    public function __construct(ServiceInterface $service)
+    {
+
+    }
+
+    public function charge(){
+
+    }
+}
+
+
