6f7b5b396759dc25d044f60fa48442248875d271
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-07-10 14:15:19 +0700

    Add business login

--- src/Services/TMPay.php
+++ src/Services/TMPay.php
@@ -13,7 +13,29 @@ class TMPay extends  Service implements ServiceInterface  {
 
     public function charge($trueMoneyNumber)
     {
+        if(function_exists('curl_init'))
+        {
+            $curl = curl_init('https://www.tmpay.net/TPG/backend.php?merchant_id=TMPAY&password='.$trueMoneyNumber. '&resp_url= http://www.mywebsite.com/tmpay_result.php');
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_HEADER, FALSE);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            $curl_content = curl_exec($curl);
+            curl_close($curl);
 
+        }else{
+            $curl_content = file_get_contents('http://www.tmpay.net/TPG/backend.php?merchant_id=TMPAY&password=' . $trueMoneyNumber .'&resp_url= http://www.mywebsite.com/tmpay_result.php');
+        }
+
+        if(strpos($curl_content,'SUCCEED') !== FALSE)
+        {
+            return true;
+
+        }  else
+        {
+            return false;
+        }
     }
 
 
